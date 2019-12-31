3fa41d36b942298b7dd11b5119115c0962600326
iideetee <joni.nurminen@comatec.fi>
2019-12-31 20:08:51 +0200

    aes

---
+++ osa3/puhelinluettelo_ja_tietokanta/step2/build/precache-manifest.a35d3a4cae5f2b4d52ae7148e73a47f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1bc2f924cb202736f234f8a2dedda22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6b523675be24e1ec8ac",
+    "url": "/static/css/main.e4202ffa.chunk.css"
+  },
+  {
+    "revision": "71650965e93258234495",
+    "url": "/static/js/2.94f60eb9.chunk.js"
+  },
+  {
+    "revision": "a6b523675be24e1ec8ac",
+    "url": "/static/js/main.c6526de9.chunk.js"
+  },
+  {
+    "revision": "22c9b7de242c44576be7",
+    "url": "/static/js/runtime-main.296685ae.js"
+  }
+]);
---
+++ osa3/puhelinluettelo_ja_tietokanta/step3/build/precache-manifest.b00a789b53f24527f1fdc256a7df1a33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f35f99bacfbb597445430e8ce9ba5a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2edc3bb35e6a33ecf63",
+    "url": "/static/css/main.e4202ffa.chunk.css"
+  },
+  {
+    "revision": "71650965e93258234495",
+    "url": "/static/js/2.94f60eb9.chunk.js"
+  },
+  {
+    "revision": "f2edc3bb35e6a33ecf63",
+    "url": "/static/js/main.1e26fdac.chunk.js"
+  },
+  {
+    "revision": "22c9b7de242c44576be7",
+    "url": "/static/js/runtime-main.296685ae.js"
+  }
+]);
